d544273116fdddb1875fd58ab47a4ff9dd4180ee
NoobCoderAk <abidakbar0001@gmail.com>
2023-06-04 20:07:27 +0700

    menambahkan fitur add contact

---
+++ lib/view/contact.dart
@@ -0,0 +1,86 @@
+import 'package:app_contact/controller/contact_controller.dart';
+import 'package:app_contact/view/add_contact.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Contact extends StatefulWidget {
+  const Contact({super.key});
+
+  @override
+  State<Contact> createState() => _ContactState();
+}
+
+class _ContactState extends State<Contact> {
+  var cc = ContactController();
+
+  @override
+  void initState() {
+    cc.getContact();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            const Text(
+              'Contact List',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+                child: StreamBuilder<List<DocumentSnapshot>>(
+              stream: cc.stream,
+              builder: ((context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                final List<DocumentSnapshot> data = snapshot.data!;
+                return ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(3),
+                      child: Card(
+                        elevation: 8,
+                        child: ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.blue,
+                            child: Text(
+                              data[index]['name'].substring(0, 1).toUpperCase(),
+                              style:
+                                  const TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          title: Text(data[index]['name']),
+                          subtitle: Text(data[index]['phone']),
+                          trailing: IconButton(
+                              onPressed: (() {}),
+                              icon: const Icon(Icons.delete)),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }),
+            ))
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (() {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: ((context) => const AddContact()),
+            ),
+          );
+        }),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
